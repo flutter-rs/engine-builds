f5974c35a5bb2fa4eb5bf31a5ec71c3f159b5fb1
Chandler Newman <chandler2newman@hotmail.co.uk>
2019-12-28 00:14:52 +0000

    Add libicudtl.a generation

--- build.sh
+++ build.sh
@@ -28,8 +28,13 @@ mkdir $OUT_PATH
 
 export SRC_OUT_PATH="${ENGINE_PATH}/src/out/host_release"
 
+echo Generating icudtl
+cp $ENGINE_PATH/src/third_party/icu/flutter/icudtl.dat $OUT_PATH/icudtl.dat
+$ENGINE_PATH/src/flutter/sky/tools/objcopy.py --objcopy objcopy --input $OUT_PATH/icudtl.dat --output $OUT_PATH/icudtl.o --arch x64
+ar rcs $OUT_PATH/libicudtl.a $OUT_PATH/icudtl.o
+
+echo Copying other outputs
 cp $SRC_OUT_PATH/libflutter_engine.so $OUT_PATH/libflutter_engine.so
-cp $SRC_OUT_PATH/icudtl.dat $OUT_PATH/icudtl.dat
 cp $SRC_OUT_PATH/flutter_embedder.h $OUT_PATH/flutter_embedder.h
 cp $SRC_OUT_PATH/dart $OUT_PATH/dart
 cp -r $SRC_OUT_PATH/flutter_patched_sdk $OUT_PATH/flutter_patched_sdk
