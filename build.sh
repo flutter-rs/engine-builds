944cde389479e44d8785b181d664badbe9fcd606
Chandler Newman <chandler2newman@hotmail.co.uk>
2020-01-13 15:00:46 +0000

    Copy lib on windows

--- build.sh
+++ build.sh
@@ -19,7 +19,8 @@ export SRC_OUT_PATH="${ENGINE_PATH}/src/out/${TARGET}"
 echo Copying other outputs
 cp $SRC_OUT_PATH/libflutter_engine.so $OUT_PATH/libflutter_engine.so ||
 cp $SRC_OUT_PATH/libflutter_engine.dylib $OUT_PATH/libflutter_engine.dylib ||
-cp $SRC_OUT_PATH/flutter_engine.dll $OUT_PATH/flutter_engine.dll
+cp $SRC_OUT_PATH/flutter_engine.dll $OUT_PATH/flutter_engine.dll &&
+cp $SRC_OUT_PATH/flutter_engine.dll $OUT_PATH/flutter_engine.dll.lib
 cp $SRC_OUT_PATH/flutter_embedder.h $OUT_PATH/flutter_embedder.h
 cp $SRC_OUT_PATH/dart $OUT_PATH/dart || :
 cp -r $SRC_OUT_PATH/flutter_patched_sdk $OUT_PATH/flutter_patched_sdk || :
